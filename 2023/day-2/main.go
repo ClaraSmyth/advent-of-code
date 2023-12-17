f8c6b692d6bde120300c9b18ea4fd2ffa6c77e89
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2023-12-17 11:16:34 +0000

    Solved Day 2 - Part 1

---
+++ 2023/day-2/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	answerPart1 := part1(lines)
+	answerPart2 := part2(lines)
+
+	println("Part 1 -", answerPart1)
+	println("Part 2 -", answerPart2)
+}
+
+func part1(lines []string) string {
+	maxRed := 12
+	maxGreen := 13
+	maxBlue := 14
+
+	sumOfGames := 0
+
+	for i, v := range lines {
+
+		if v == "" {
+			continue
+		}
+
+		pass := true
+
+		currentId := i + 1
+
+		stringWithoutId := strings.Split(v, ":")[1]
+
+		allGames := strings.Split(stringWithoutId, ";")
+
+		for _, game := range allGames {
+
+			red := 0
+			green := 0
+			blue := 0
+
+			fieldsSlice := strings.Fields(game)
+
+			for j := 0; j < len(fieldsSlice)/2; j++ {
+				numString := fieldsSlice[j*2]
+				colorString := fieldsSlice[(j*2)+1]
+
+				num, err := strconv.Atoi(numString)
+				if err != nil {
+					panic(err)
+				}
+
+				if strings.Contains(colorString, "red") {
+					red += num
+				}
+
+				if strings.Contains(colorString, "green") {
+					green += num
+				}
+
+				if strings.Contains(colorString, "blue") {
+					blue += num
+				}
+			}
+
+			if red > maxRed || green > maxGreen || blue > maxBlue {
+				pass = false
+			}
+
+		}
+
+		if pass {
+			sumOfGames += currentId
+		}
+	}
+
+	return fmt.Sprintf("%d", sumOfGames)
+}
+
+func part2(lines []string) string {
+	return "Not finished"
+}
