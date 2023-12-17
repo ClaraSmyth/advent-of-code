8259b5c19dde099ac1ed8a9a26a874247e69910b
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2023-12-17 08:03:20 +0000

    Solved Day 1 - Part 2

--- 2023/day-1/main.go
+++ 2023/day-1/main.go
@@ -17,22 +17,30 @@ func main() {
 
 	lines := strings.Split(string(data), "\n")
 
-	var total int64 = 0
+	answerPart1 := part1(lines)
+	answerPart2 := part2(lines)
+
+	println("Part 1 -", answerPart1)
+	println("Part 2 -", answerPart2)
+}
+
+func part1(lines []string) int {
+	total := 0
 
 	for _, v := range lines {
 
-		firstNum := strings.IndexFunc(v, unicode.IsNumber)
-		secondNum := strings.LastIndexFunc(v, unicode.IsNumber)
+		firstIndex := strings.IndexFunc(v, unicode.IsNumber)
+		secondIndex := strings.LastIndexFunc(v, unicode.IsNumber)
 
-		if firstNum == -1 || secondNum == -1 {
+		if firstIndex == -1 || secondIndex == -1 {
 			// If no number in string break out
 			break
 		} else {
 			// Get both numbers and combine them
 
-			numAsString := string(v[firstNum]) + string(v[secondNum])
+			numAsString := string(v[firstIndex]) + string(v[secondIndex])
 
-			num, err := strconv.ParseInt(numAsString, 10, 64)
+			num, err := strconv.Atoi(numAsString)
 			if err != nil {
 				panic(err)
 			}
@@ -41,5 +49,75 @@ func main() {
 		}
 	}
 
-	fmt.Println(total)
+	return total
+}
+
+func part2(lines []string) int {
+	total := 0
+
+	for _, v := range lines {
+
+		numAsString := getNumberFromString(v)
+
+		if numAsString == "-1-1" {
+			continue
+		}
+
+		num, err := strconv.Atoi(numAsString)
+		if err != nil {
+			panic(err)
+		}
+
+		total += num
+	}
+
+	return total
+}
+
+func getNumberFromString(s string) string {
+	words := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	smallestIndex := strings.IndexFunc(s, unicode.IsNumber)
+	largestIndex := strings.LastIndexFunc(s, unicode.IsNumber)
+
+	smallestNumber := -1
+	largestNumber := -1
+
+	if smallestIndex != -1 {
+
+		num1, err := strconv.Atoi(string(s[smallestIndex]))
+		if err != nil {
+			panic(err)
+		}
+
+		num2, err := strconv.Atoi(string(s[largestIndex]))
+		if err != nil {
+			panic(err)
+		}
+
+		smallestNumber = num1
+		largestNumber = num2
+	}
+
+	for i, v := range words {
+
+		firstWordIndex := strings.Index(s, v)
+		lastWordIndex := strings.LastIndex(s, v)
+
+		if firstWordIndex == -1 || lastWordIndex == -1 {
+			continue
+		}
+
+		if smallestIndex == -1 || smallestIndex >= firstWordIndex {
+			smallestIndex = firstWordIndex
+			smallestNumber = i + 1
+		}
+
+		if largestIndex == -1 || largestIndex <= lastWordIndex {
+			largestIndex = lastWordIndex
+			largestNumber = i + 1
+		}
+	}
+
+	return fmt.Sprintf("%d%d", smallestNumber, largestNumber)
 }
