9493ec28ba19a6ed7c9357439820aeb93baf1b34
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2023-12-20 21:54:18 +0000

    Solved Day 6 - Part 2

--- 2023/day-6/part2.go
+++ 2023/day-6/part2.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func part2(lines []string) string {
-	return "Not finished"
+	time := stringToNum(strings.Join(strings.Fields(strings.Split(lines[0], ":")[1]), ""))
+	dist := stringToNum(strings.Join(strings.Fields(strings.Split(lines[1], ":")[1]), ""))
+
+	min := 0
+
+	for j := 0; j < time; j++ {
+		if j*(time-j) > dist {
+			break
+		}
+		min += 1
+	}
+
+	total := time - min*2 + 1
+
+	return fmt.Sprintf("%d", total)
 }
