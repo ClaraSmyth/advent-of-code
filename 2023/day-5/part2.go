731c29015570cf1a103b280ad942efb85cc5a4d4
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2023-12-20 12:28:55 +0000

    Solved Day 5 - Part 2 (Slow - 5 min run time)

--- 2023/day-5/part2.go
+++ 2023/day-5/part2.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func part2(lines []string) string {
-	return "Not finished"
+	seeds, maps := formatLines(lines)
+
+	finalValues := []int{}
+
+	for i := 0; i < len(seeds)/2; i++ {
+		value1 := seeds[i*2]
+		value2 := seeds[(i*2)+1]
+
+		lowestOutput := checkSeed(value1, maps)
+
+		for j := 0; j < value2; j++ {
+			output := checkSeed(value1+j, maps)
+
+			if output < lowestOutput {
+				lowestOutput = output
+			}
+		}
+
+		finalValues = append(finalValues, lowestOutput)
+	}
+
+	return fmt.Sprintf("%d", slices.Min(finalValues))
+}
+
+func checkSeed(seed int, maps [][][]int) int {
+	output := seed
+
+	for _, currentMap := range maps {
+		for _, line := range currentMap {
+			if output >= line[1] && output < (line[1]+line[2]) {
+				output = output + (line[0] - line[1])
+				break
+			}
+		}
+	}
+
+	return output
 }
